aa7aeea72db45f616f515541d8351de9e8be4efd
srilu396 <srilunagulapalli396@gmail.com>
2026-05-23 21:26:46 +0530

    Optimize Vite build configuration

--- README.md
+++
@@ -1,1 +0,0 @@
-# Team-Task-Manager-frontend
--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,15 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   },
 });
