992a25677166d5fe6f20d307d379c4e88844a7c9
srilu396 <srilunagulapalli396@gmail.com>
2026-07-27 22:02:12 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -10,14 +10,5 @@ export default defineConfig({
   build: {
     outDir: 'build',
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
   },
 });
